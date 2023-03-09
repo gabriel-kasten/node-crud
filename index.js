71ae637f76df3388808bd075c671194b5d0df4cf
Gabriel Kasten <gabrieldegkasten@gmail.com>
2023-03-09 17:42:30 -0300

    added ejs, nodemon and body-parser node modules.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-git config --global user.email "gabrieldegkasten@gmail.com"
-git config --global user.name "Gabriel Kasten"
+// git config --global user.email "gabrieldegkasten@gmail.com"
+// git config --global user.name "Gabriel Kasten"
+
+const express = require('express');
+const app = express();
+
+
+
+app.listen(8080, () => {
+    console.log('Server rodando');
+})
